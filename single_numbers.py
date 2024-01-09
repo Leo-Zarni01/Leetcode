5164a9170c7e7112562c2f03a72e20c2cf243feb
Leo-Zarni01 <leokhantzarni@gmail.com>
2024-01-09 18:12:04 +1100

    finished

---
+++ single_numbers.py
@@ -0,0 +1,17 @@
+from typing import List
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        i = 0
+        counts = {}
+        while i < len(nums):
+            if nums[i] not in counts:
+                counts[nums[i]] = 1
+            else:
+                counts[nums[i]] += 1
+            i += 1
+
+        for key in counts.keys():
+            if counts[key] == 1:
+                return key
+        
