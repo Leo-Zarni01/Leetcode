8b4a7dfcd046afee873449107b58ee4c53690254
Leo Zarni <leokhantzarni@gmail.com>
2024-10-14 22:48:57 +1100

    finished encode decode

---
+++ exercises/encode_decode.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        combined_str = ""
+        for each_string in strs:
+            combined_str += each_string
+            combined_str += "\n"
+        return combined_str
+
+    def decode(self, s: str) -> List[str]:
+        print("To analyze: ", s)
+        strings = []
+        current_charc = ""
+        for each_character in s:
+            if each_character == "\n":
+                print("To add to list: ", current_charc)
+                strings.append(current_charc)
+                current_charc = ""
+            else:
+                current_charc += each_character
+        return strings
+
+example_1 = Solution()
+example_1_encode = Solution().encode(["neet","code","love","you"])
+example_1_decode = Solution().decode(example_1_encode)
+print(example_1_decode)
