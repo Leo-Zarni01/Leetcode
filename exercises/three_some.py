804bcacc287420d1b9c99caede9e34751424f621
Leo-Zarni01 <leokhantzarni@gmail.com>
2024-11-30 22:25:57 +1100

    threesum solved (optimize later)

--- exercises/three_some.py
+++ exercises/three_some.py
@@ -9,7 +9,7 @@ class Solution:
             right_num = numbers[right]
             print("Left num is: ", left_num)
             print("Right num is: ", right_num)
-            if left > right:
+            if left >= right:
                 break
             else:
                 if left_num + right_num > target:
@@ -26,11 +26,13 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         solutions = []
         nums.sort()
+        triplet_tracker = {}
 
         for index in range(len(nums)):
+            print("Sorted array is: ", nums)
             curr = nums[index]
             print("Current number is: ", curr)
-            sol = [curr]
+
             target = 0 - curr
             print("Target is: ", target)
             look_up_arr = nums[index + 1:]
@@ -39,39 +41,50 @@ class Solution:
             print(valid_pairs)
             if len(valid_pairs) > 0:
                 for each_pair in valid_pairs:
-                    print(each_pair)
-                    ## first_number = each_pair[0] + 1
-                    ## second_number = each_pair[1] + 1
-                    ## sol.append(first_number)
-                    ## sol.append(second_number)
-                    ## if sol not in solutions:
-                    ##     solutions.append(sol)
+                    first_number = nums[each_pair[0] + index + 1]
+                    second_number = nums[each_pair[1] + index + 1]
+                    sol = (curr, first_number, second_number)
+                    print("Triplet so far is: ", sol)
+                    if sol not in triplet_tracker:
+                        triplet_tracker[sol] = True
+            print("Triplets: ", triplet_tracker)
             print()
+
+        for each_triplet in triplet_tracker:
+            first = each_triplet[0]
+            second = each_triplet[1]
+            third = each_triplet[2]
+            triplet_list = [first, second, third]
+            solutions.append(triplet_list)
+
         return solutions
 
-## demo_one = Solution()
-## nums=[-1,0,1,2,-1,-4]
-## test_one = demo_one.threeSum(nums)
-## print(test_one)
-## print()
-## 
-## print("BREAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAK")
-## print()
-## 
-## demo_two = Solution()
-## nums_two=[0, 0, 0]
-## test_two = demo_two.threeSum(nums_two)
-## print(test_two)
-## 
-## print("BREAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAK")
-## print()
+demo_one = Solution()
+nums=[-1,0,1,2,-1,-4]
+test_one = demo_one.threeSum(nums)
+print(test_one)
+print()
 
-## demo_three = Solution()
-## nums_three=[0, 0, 0, 0]
-## test_three = demo_three.threeSum(nums_three)
-## print(test_three)
+# print("BREAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAK")
+# print()
 
-demo_four = Solution()
-nums_four = [-2,0,1,1,2]
-test_four = demo_four.threeSum(nums_four)
-print(test_four)
+# demo_two = Solution()
+# nums_two=[0, 0, 0]
+# test_two = demo_two.threeSum(nums_two)
+# print(test_two)
+
+# print("BREAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAK")
+# print()
+
+# demo_three = Solution()
+# nums_three=[0, 0, 0, 0]
+# test_three = demo_three.threeSum(nums_three)
+# print(test_three)
+
+# print("BREAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAK")
+# print()
+
+# demo_four = Solution()
+# nums_four = [-2,0,1,1,2]
+# test_four = demo_four.threeSum(nums_four)
+# print(test_four)
